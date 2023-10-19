23f264520f7612343171aae8e3c662123425a31b
genivalc <genivalcandeiadon.neto@gmail.com>
2023-10-19 10:09:46 -0300

    fix

--- nestjs/dist/bank-accounts/bank-accounts.service.d.ts
+++
@@ -1,9 +0,0 @@
-import { CreateBankAccountDto } from './dto/create-bank-account.dto';
-import { UpdateBankAccountDto } from './dto/update-bank-account.dto';
-export declare class BankAccountsService {
-    create(createBankAccountDto: CreateBankAccountDto): string;
-    findAll(): string;
-    findOne(id: number): string;
-    update(id: number, updateBankAccountDto: UpdateBankAccountDto): string;
-    remove(id: number): string;
-}
--- nestjs/dist/bank-accounts/dto/create-bank-account.dto.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.CreateBankAccountDto = void 0;
-class CreateBankAccountDto {
-}
-exports.CreateBankAccountDto = CreateBankAccountDto;
-//# sourceMappingURL=create-bank-account.dto.js.map
